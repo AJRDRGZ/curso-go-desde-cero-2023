55e1a2dcba59ddbeb3382a32ed547c8415df6dbf
Alejandro Rodríguez <aj.softwaredeveloper@gmai.com>
2023-02-15 15:24:37 -0500

    Comentarios

---
+++ 3_comentarios/main.go
@@ -0,0 +1,25 @@
+// package main es el paquete principal
+// en donde se ejecuta nuestra aplicación
+// de EDteam
+package main
+
+// Domain es el sitio oficial de EDteam
+const Domain = "ed.team"
+
+// Name es el nombre del profe de Go
+var Name = "alejandro"
+
+// estas son las constantes de los meses
+const (
+	Jan = iota + 1
+	Feb
+	Mar
+	Abr
+	May
+	Jun
+)
+
+/* func main() {
+
+}
+*/
