c6f82ea439d4fdd605d95e3268109d0e459d5f70
Alejandro Rodríguez <aj.softwaredeveloper@gmai.com>
2023-02-27 22:44:42 -0500

    Arrays

---
+++ 3_2_arrays/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var flags [3]string
+	// flags[0] = "🎏"
+	// flags[1] = "🏴"
+	// flags[2] = "🏳️"
+
+	flags := [...]string{"🎏", "🏴", "🏳️", "🏴‍☠️", "🏴"}
+
+	fmt.Println(flags)
+}
