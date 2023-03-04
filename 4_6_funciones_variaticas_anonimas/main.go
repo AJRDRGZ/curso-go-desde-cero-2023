216f232f506044e115f285dd37b6992d8a0a579e
Alejandro Rodríguez <aj.softwaredeveloper@gmai.com>
2023-03-03 20:26:59 -0500

    funciones anonimas y variatica

---
+++ 4_6_funciones_variaticas_anonimas/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println(sum(2))
+	// fmt.Println(sum(2, 3))
+	// fmt.Println(sum(2, 3, 12))
+	// fmt.Println(sum(2, 3, 12, 1, 24))
+
+	func(name string) {
+		fmt.Println("✋ Hello", name)
+	}("gophers")
+
+}
+
+// func sum(nums ...int) (total int) {
+// 	for _, num := range nums {
+// 		total += num
+// 	}
+// 	return
+// }
