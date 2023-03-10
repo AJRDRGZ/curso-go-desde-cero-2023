568143ae72cc451b1aaf1b91f8b6bb4e0744d889
Alejandro Rodríguez <aj.softwaredeveloper@gmai.com>
2023-03-10 16:47:21 -0500

    Parametros de tipo

---
+++ 5_2_parametros_de_tipo/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type MyInt int
+type MyIntV2 int
+
+func main() {
+	var num1 MyInt = 2
+	var num2 MyInt = 6
+
+	var num3 MyIntV2 = 3
+	var num4 MyIntV2 = 4
+
+	fmt.Println(sum(2, 4, 67))
+	fmt.Println(sum(2.0, 4.6, 67.1))
+	fmt.Println(sum(num1, num2))
+	fmt.Println(sum(num3, num4))
+}
+
+func sum[T ~int | float64](nums ...T) T {
+	var total T
+	for _, num := range nums {
+		total += num
+	}
+
+	return total
+}
